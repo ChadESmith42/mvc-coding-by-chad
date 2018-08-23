e0b1088c529288f6ba9117d97a0bde1eab91b069
Chad Smith <chad.e.smith@outlook.com>
2018-08-23 11:42:32 -0500

    COMMIT: Cleanup to bring project up to date.

--- PortfolioProjectMVC/DangerousBuildings/dangerousbuildings.js
+++ PortfolioProjectMVC/DangerousBuildings/dangerousbuildings.js
@@ -11,6 +11,9 @@ var map = new mapboxgl.Map({
 //Variable to hold GeoJSON object link from Live Data Feed;
 var url = "https://data.kcmo.org/resource/rm2v-mbk5.geojson";
 
+//Variable to hold address. Address will be assigned in map layer and used to call
+//REST service for parcel data.
+//var lookup = "";
 map.on("load", function () {
     window.setInterval(function () {
         map.getSource("home").setData(url);
@@ -36,10 +39,15 @@ map.on("load", function () {
                 "icon-image": "custom-marker"
             }
         });
+        //Create variable that will be passed to Angular model for populating table of
+        //location-based query. Variable will be integrated into HTTP GET request on
+        //REST service.
+        
+
         //Adds popup triggered by a click on the marker:
         map.on("click", "home", function (e) {
             var coordinates = e.features[0].geometry.coordinates.slice();
-
+            var lookup = e.features[0].properties.address;
             while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
                 coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
             }
@@ -65,3 +73,17 @@ map.on("load", function () {
         });
     });
 });
+
+var parcelApp = angular.module('parcelApp', []);//module name and dependencies []
+parcelApp.controller('ownerController', function ($scope, $http) {
+    $scope.response = null;
+    /* $HTTP SERVICE:
+    The $http service issues a GET request to our provided url which returns a
+    JSON result of all of our titles the "then" statement takes the results and
+    places them in the response.data object and passes it to the view to be used.
+    */
+    $http({ method: 'GET', url: 'https://data.kcmo.org/resource/9skc-zsy8.geojson?address=' + lookup })//end $http() service
+    .then(function (response) {
+        $scope.data = response.data;
+    });//end then() event handler
+});//end of controller
